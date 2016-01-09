09c524999f54ce3685a41a318254238e01652820
a3test <a3test@163.com>
2016-01-09 16:42:21 +0800

    V1.0

---
+++ Hash/HashService.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace Hash
+{
+    public class HashService
+    {
+        /// <summary>
+        /// 获取文件 MD5 值
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public string GetFileMd5(string filePath)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            byte[] hash = md5.ComputeHash(fs);
+            fs.Close();
+
+            string ret = "";
+            foreach (byte b in hash)
+            {
+                ret += b.ToString("X2");
+            }
+            return ret;
+        }
+
+        public string GetFileSha1(string filePath)
+        {
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            byte[] hash = sha1.ComputeHash(fs);
+            fs.Close();
+
+            string ret = "";
+            foreach (byte b in hash)
+            {
+                ret += b.ToString("X2");
+            }
+            return ret;
+        }
+    }
+}
---
+++ Hash/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Hash
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HashService service = new HashService();
+
+            Console.WriteLine("----------------------------------------------");
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("错误:请指定文件名。");
+            }
+            else if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("错误:指定的文件不存在。");
+            }
+            else
+            {
+                Console.WriteLine("文件:{0}", args[0]);
+                Console.WriteLine(" MD5:{0}", service.GetFileMd5(args[0]));
+                Console.WriteLine("SHA1:{0}", service.GetFileSha1(args[0]));
+            }
+            
+            Console.WriteLine("----------------------------------------------");
+        }
+    }
+}
